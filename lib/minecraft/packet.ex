efda80371930eeca289e5abb38b4289b1fbc470b
Michael Oliver <mikemboliver@gmail.com>
2018-06-28 00:47:14 -0700

    Rename deserialize -> decode

--- lib/minecraft/packet.ex
+++ lib/minecraft/packet.ex
@@ -7,52 +7,52 @@ defmodule Minecraft.Packet do
   """
   @spec deserialize(binary, state :: atom) :: {new_state :: atom, packet :: term}
   def deserialize(data, :handshaking) when is_binary(data) do
-    {_packet_size, data} = deserialize_varint(data)
-    {packet_id, data} = deserialize_varint(data)
+    {_packet_size, data} = decode_varint(data)
+    {packet_id, data} = decode_varint(data)
     Handshake.deserialize(packet_id, data)
   end
 
   @doc """
-  Deserializes a variable-size integer.
+  Decodes a variable-size integer.
   """
-  @spec deserialize_varint(binary) ::
+  @spec decode_varint(binary) ::
           {decoded :: integer, rest :: binary} | {:error, :too_long | :too_short}
-  def deserialize_varint(data) do
-    deserialize_varint(data, 0, 0)
+  def decode_varint(data) do
+    decode_varint(data, 0, 0)
   end
 
-  def deserialize_varint(<<1::1, value::7, rest::binary>>, num_read, acc) when num_read < 5 do
-    deserialize_varint(rest, num_read + 1, acc + (value <<< (7 * num_read)))
+  def decode_varint(<<1::1, value::7, rest::binary>>, num_read, acc) when num_read < 5 do
+    decode_varint(rest, num_read + 1, acc + (value <<< (7 * num_read)))
   end
 
-  def deserialize_varint(<<0::1, value::7, rest::binary>>, num_read, acc) do
+  def decode_varint(<<0::1, value::7, rest::binary>>, num_read, acc) do
     result = acc + (value <<< (7 * num_read))
     <<result::32-signed>> = <<result::32-unsigned>>
     {result, rest}
   end
 
-  def deserialize_varint(_, num_read, _) when num_read >= 5, do: {:error, :too_long}
-  def deserialize_varint("", _, _), do: {:error, :too_short}
+  def decode_varint(_, num_read, _) when num_read >= 5, do: {:error, :too_long}
+  def decode_varint("", _, _), do: {:error, :too_short}
 
   @doc """
-  Deserializes a variable-size long.
+  Decodes a variable-size long.
   """
-  @spec deserialize_varlong(binary) ::
+  @spec decode_varlong(binary) ::
           {decoded :: integer, rest :: binary} | {:error, :too_long | :too_short}
-  def deserialize_varlong(data) do
-    deserialize_varlong(data, 0, 0)
+  def decode_varlong(data) do
+    decode_varlong(data, 0, 0)
   end
 
-  def deserialize_varlong(<<1::1, value::7, rest::binary>>, num_read, acc) when num_read < 10 do
-    deserialize_varlong(rest, num_read + 1, acc + (value <<< (7 * num_read)))
+  def decode_varlong(<<1::1, value::7, rest::binary>>, num_read, acc) when num_read < 10 do
+    decode_varlong(rest, num_read + 1, acc + (value <<< (7 * num_read)))
   end
 
-  def deserialize_varlong(<<0::1, value::7, rest::binary>>, num_read, acc) do
+  def decode_varlong(<<0::1, value::7, rest::binary>>, num_read, acc) do
     result = acc + (value <<< (7 * num_read))
     <<result::64-signed>> = <<result::64-unsigned>>
     {result, rest}
   end
 
-  def deserialize_varlong(_, num_read, _) when num_read >= 10, do: {:error, :too_long}
-  def deserialize_varlong("", _, _), do: {:error, :too_short}
+  def decode_varlong(_, num_read, _) when num_read >= 10, do: {:error, :too_long}
+  def decode_varlong("", _, _), do: {:error, :too_short}
 end
--- test/minecraft/packet_test.exs
+++ test/minecraft/packet_test.exs
@@ -4,77 +4,77 @@ defmodule Minecraft.PacketTest do
 
   describe "deserializing varints" do
     test "basics" do
-      assert {0, ""} = deserialize_varint(<<0>>)
-      assert {1, ""} = deserialize_varint(<<1>>)
-      assert {2, ""} = deserialize_varint(<<2>>)
+      assert {0, ""} = decode_varint(<<0>>)
+      assert {1, ""} = decode_varint(<<1>>)
+      assert {2, ""} = decode_varint(<<2>>)
     end
 
     test "first breakpoint" do
-      assert {127, ""} = deserialize_varint(<<0x7F>>)
-      assert {128, ""} = deserialize_varint(<<0x80, 0x01>>)
-      assert {255, ""} = deserialize_varint(<<0xFF, 0x01>>)
+      assert {127, ""} = decode_varint(<<0x7F>>)
+      assert {128, ""} = decode_varint(<<0x80, 0x01>>)
+      assert {255, ""} = decode_varint(<<0xFF, 0x01>>)
     end
 
     test "limits" do
-      assert {2_147_483_647, ""} = deserialize_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0x07>>)
-      assert {-1, ""} = deserialize_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0x0F>>)
-      assert {-2_147_483_648, ""} = deserialize_varint(<<0x80, 0x80, 0x80, 0x80, 0x08>>)
+      assert {2_147_483_647, ""} = decode_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0x07>>)
+      assert {-1, ""} = decode_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0x0F>>)
+      assert {-2_147_483_648, ""} = decode_varint(<<0x80, 0x80, 0x80, 0x80, 0x08>>)
     end
 
     test "extra data" do
-      assert {0, <<1, 2, 3>>} = deserialize_varint(<<0, 1, 2, 3>>)
-      assert {255, <<1, 2, 3>>} = deserialize_varint(<<0xFF, 0x01, 1, 2, 3>>)
-      assert {-1, <<1, 2, 3>>} = deserialize_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0x0F, 1, 2, 3>>)
+      assert {0, <<1, 2, 3>>} = decode_varint(<<0, 1, 2, 3>>)
+      assert {255, <<1, 2, 3>>} = decode_varint(<<0xFF, 0x01, 1, 2, 3>>)
+      assert {-1, <<1, 2, 3>>} = decode_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0x0F, 1, 2, 3>>)
     end
 
     test "errors" do
-      assert {:error, :too_short} = deserialize_varint(<<0xFF>>)
-      assert {:error, :too_long} = deserialize_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF>>)
+      assert {:error, :too_short} = decode_varint(<<0xFF>>)
+      assert {:error, :too_long} = decode_varint(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF>>)
     end
   end
 
   describe "deserializing varlongs" do
     test "basics" do
-      assert {0, ""} = deserialize_varlong(<<0>>)
-      assert {1, ""} = deserialize_varlong(<<1>>)
-      assert {2, ""} = deserialize_varlong(<<2>>)
+      assert {0, ""} = decode_varlong(<<0>>)
+      assert {1, ""} = decode_varlong(<<1>>)
+      assert {2, ""} = decode_varlong(<<2>>)
     end
 
     test "first breakpoint" do
-      assert {127, ""} = deserialize_varlong(<<0x7F>>)
-      assert {128, ""} = deserialize_varlong(<<0x80, 0x01>>)
-      assert {255, ""} = deserialize_varlong(<<0xFF, 0x01>>)
+      assert {127, ""} = decode_varlong(<<0x7F>>)
+      assert {128, ""} = decode_varlong(<<0x80, 0x01>>)
+      assert {255, ""} = decode_varlong(<<0xFF, 0x01>>)
     end
 
     test "limits" do
       assert {9_223_372_036_854_775_807, ""} =
-               deserialize_varlong(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F>>)
+               decode_varlong(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F>>)
 
       assert {-1, ""} =
-               deserialize_varlong(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01>>)
+               decode_varlong(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01>>)
 
       assert {-2_147_483_648, ""} =
-               deserialize_varlong(<<0x80, 0x80, 0x80, 0x80, 0xF8, 0xFF, 0xFF, 0xFF, 0xFF, 0x01>>)
+               decode_varlong(<<0x80, 0x80, 0x80, 0x80, 0xF8, 0xFF, 0xFF, 0xFF, 0xFF, 0x01>>)
 
       assert {-9_223_372_036_854_775_808, ""} =
-               deserialize_varlong(<<0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01>>)
+               decode_varlong(<<0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01>>)
     end
 
     test "extra data" do
-      assert {0, <<1, 2, 3>>} = deserialize_varlong(<<0, 1, 2, 3>>)
-      assert {255, <<1, 2, 3>>} = deserialize_varlong(<<0xFF, 0x01, 1, 2, 3>>)
+      assert {0, <<1, 2, 3>>} = decode_varlong(<<0, 1, 2, 3>>)
+      assert {255, <<1, 2, 3>>} = decode_varlong(<<0xFF, 0x01, 1, 2, 3>>)
 
       assert {-1, <<1, 2, 3>>} =
-               deserialize_varlong(
+               decode_varlong(
                  <<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0F, 1, 2, 3>>
                )
     end
 
     test "errors" do
-      assert {:error, :too_short} = deserialize_varlong(<<0xFF>>)
+      assert {:error, :too_short} = decode_varlong(<<0xFF>>)
 
       assert {:error, :too_long} =
-               deserialize_varlong(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF>>)
+               decode_varlong(<<0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF>>)
     end
   end
 end
